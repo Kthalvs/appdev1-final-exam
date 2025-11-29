c283efffad5b1db6e439c0a58185445913c5df2c
Kthalv <alvaradokathline@gmail.com>
2025-11-29 14:06:49 +0800

    Done with connect React router to the app section

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const basename = "/appdev1-final-exam";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter basename={basename}>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
+
