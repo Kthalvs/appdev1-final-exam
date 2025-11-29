1c7bd40ed552177c31fca6a1e0688902907d3fba
Kthalv <alvaradokathline@gmail.com>
2025-11-29 13:18:47 +0800

    Implement TodoItem component

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+function TodoItem({ todo, theme, onToggle, onDelete }) {
+  const [isFalling, setIsFalling] = useState(false);
+
+  const handleDeleteClick = () => {
+    setIsFalling(true);
+  };
+
+  const handleTransitionEnd = () => {
+    if (isFalling) {
+      onDelete(todo.id);
+    }
+  };
+
+  return (
+    <div
+      className={`todo ${theme}-todo ${
+        todo.completed ? "completed" : ""
+      } ${isFalling ? "fall" : ""}`}
+      onTransitionEnd={handleTransitionEnd}
+    >
+      <li className="todo-item">{todo.title}</li>
+      <button
+        className={`check-btn ${theme}-button`}
+        onClick={() => onToggle(todo)}
+      >
+        <i className="fas fa-check"></i>
+      </button>
+      <button
+        className={`delete-btn ${theme}-button`}
+        onClick={handleDeleteClick}
+      >
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  );
+}
+
+export default TodoItem;
